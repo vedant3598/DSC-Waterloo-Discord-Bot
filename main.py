d4fe5f227808e26ef0d074386bbf8e2fde914773
Vedant Shah <vedant18@hotmail.com>
2021-05-30 22:19:55 -0400

    Starting works of discord bot

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import discord
+import os
 
 client = discord.Client()
+# Add Discord Token here when bot is ready to be deployed
+TOKEN = os.getenv('')
+
+@client.event
+async def on_ready():
+    print('{} is connected & ready to rumble!'.format(client.user))
+
+@client.event
+async def on_message(message):
+    print()
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        
+    )
+
+
+
+client.run()
