e6508eb1e2c1f000a6bb1e954a7d705eb7cd82b9
Vedant Shah <vedant18@hotmail.com>
2021-05-31 23:38:09 -0400

    Adding DSC bot commands

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 import discord
 import os
+from discord.ext import commands
 
-PATH = ''
 client = discord.Client()
 # Add Discord Token here when bot is ready to be deployed
-TOKEN = os.getenv(PATH)
+TOKEN = os.getenv('')
 
-@client.event
+DSC_bot = commands.Bot(command_prefix='/')
+
+@DSC_bot.event
 async def on_ready():
-    print('{} is connected & ready to rumble!'.format(client.user))
+    print('{} is connected & ready to rumble!'.format(DSC_bot.user.name))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# scrapes Google to find GCP documentation link
+@DSC_bot.command(name='Google Cloud Platform')
+async def gcp(ctx):
+    response = ""
+    await ctx.send(response)
+
+# scrapes Google to find Google Internships
+@DSC_bot.command(name='Google Internships')
+async def gcp(ctx):
+    response = ""
+    await ctx.send(response)
+
+# returns information about the server
+@DSC_bot.command(name='server info')
+async def gcp(ctx):
+    response = ""
+    await ctx.send(response)
 
     
 
-@client.event
+@DSC_bot.event
 async def on_member_join(member):
-    channel = client.get_channel(837526205540073506)
-    intro_channel = client.get_channel(848759566635499570)
+    channel = DSC_bot.get_channel(837526205540073506)
+    intro_channel = DSC_bot.get_channel(848759566635499570)
     # Figure out how to link other channels in message
     await channel.send(
         "Hello {}! Welcome to the Google Developer Student Clubs, University of Waterloo chapter discord server." +
@@ -30,4 +45,4 @@ async def on_member_join(member):
         "Say hi and introduce yourself in the introduction channel #" + intro_channel + "!"
     )
 
-client.run()
+client.run(TOKEN)
