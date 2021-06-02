c7d673fa6d76f6c4a5d4bb2920bcb3fbb3c9eb0d
Vedant Shah <vedant18@hotmail.com>
2021-06-01 23:53:34 -0400

    Adding content for server info

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ DSC_bot = commands.Bot(command_prefix='/')
 async def on_ready():
     print('{} is connected & ready to rumble!'.format(DSC_bot.user.name))
 
-# scrapes Google to find GCP documentation link
-@DSC_bot.command(name='Google Cloud Platform')
-async def gcp(ctx):
-    response = ""
-    await ctx.send(response)
-
-
 # scrapes Google to find Google Developer tools
 @DSC_bot.command(name='Google Dev Tools')
 async def gcp(ctx):
@@ -37,9 +30,21 @@ async def server_info(ctx):
     guild = DSC_bot.fetch_guild(837526205540073502)
     channels = await guild.fetch_channels()
     members = await guild.fetch_members().flatten()
-    roles = await guild.fetch_roles().flatten()
+    
+    channels_message = discord.Embed(
+        title = 'Channels',
+        description = channels,
+        colour = discord.Colour.orange()
+    )
 
-    await ctx.send("")
+    members_message = discord.Embed(
+        title = 'Members',
+        description = members,
+        colour = discord.Colour.orange()
+    )
+
+    await ctx.send(channels_message)
+    await ctx.send(members_message)
 
 @DSC_bot.event
 async def on_member_join(member):
